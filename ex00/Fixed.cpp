02419771f2adb0a3fff9468d6fc94436b20ea91e
Crysicia <7459685+Crysicia@users.noreply.github.com>
2021-11-23 15:11:53 +0100

    Add default rawBits value

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -1,18 +1,27 @@
 #include "Fixed.hpp"
 
-Fixed::Fixed() {}
-Fixed::Fixed(Fixed& copy) {
-	this->rawBits = copy.getRawBits();
+Fixed::Fixed() {
+	std::cout << "Defaut constructor called" << std::endl;
+	this->rawBits = 0;
 }
-Fixed::~Fixed() {}
+Fixed::Fixed(Fixed& copy) {
+	std::cout << "Copy constructor called" << std::endl;
+	*this = copy;
+}
+Fixed::~Fixed() {
+	std::cout << "Defaut destructor called" << std::endl;
+}
 
 Fixed& Fixed::operator= (const Fixed& fixed) {
+	std::cout << "Assignation operator called" << std::endl;
 	this->rawBits = fixed.getRawBits();
 	return *this;
 }
 int Fixed::getRawBits(void) const {
+	std::cout << "GetRawBits function called" << std::endl;
 	return this->rawBits;
 }
 void Fixed::setRawBits(int const data) {
+	std::cout << "SetRawBits called" << std::endl;
 	this->rawBits = data;
 }
